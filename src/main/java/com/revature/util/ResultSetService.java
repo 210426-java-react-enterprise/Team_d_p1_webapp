f0f725272d08e2f475bb154cd0d6794e64432875
diuguide <everett.diuguid@gmail.com>
2021-05-21 10:31:06 -0400

    added hashmap for user

--- src/main/java/com/revature/util/ResultSetService.java
+++ src/main/java/com/revature/util/ResultSetService.java
@@ -9,7 +9,7 @@ public class ResultSetService {
 
     private ResultSet rs;
 
-    public LinkedList<HashMap> resultSetToLinkedList(ResultSet rs) throws SQLException {
+    public LinkedList<HashMap> resultSetToLinkedListTask(ResultSet rs) throws SQLException {
         HashMap<String, Object> resultMap = new HashMap<>();
         LinkedList<HashMap> taskList = new LinkedList<>();
         while(rs.next()) {
@@ -18,6 +18,21 @@ public class ResultSetService {
             resultMap.put("message", rs.getString("message"));
             resultMap.put("dueDate", rs.getString("dueDate"));
             resultMap.put("taskState", rs.getBoolean("taskState"));
+            taskList.add(resultMap);
+        }
+        return taskList;
+    }
+
+    public LinkedList<HashMap> resultSetToLinkedListUser(ResultSet rs) throws SQLException {
+        HashMap<String, Object> resultMap = new HashMap<>();
+        LinkedList<HashMap> taskList = new LinkedList<>();
+        while(rs.next()) {
+            resultMap.put("username", rs.getInt("username"));
+            resultMap.put("password", rs.getString("password"));
+            resultMap.put("firstName", rs.getString("firstName"));
+            resultMap.put("lastName", rs.getString("lastName"));
+            resultMap.put("email", rs.getBoolean("email"));
+            resultMap.put("age", rs.getInt("age"));
             taskList.add(resultMap);
         }
         return taskList;
