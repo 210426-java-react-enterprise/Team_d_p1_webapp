227af595f6a13d984e45f5690b53be277a54a008
annaisha-sys <83236719+annaisha-sys@users.noreply.github.com>
2021-05-21 23:21:47 -0400

    connection factory
    updated connection factory

---
+++ src/main/java/com/revature/utils/ConnectionFactory.java
@@ -0,0 +1,42 @@
+package com.revature.utils;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionFactory {
+    private static ConnectionFactory connectionFactory;
+    private static Properties props = new Properties();
+
+    private ConnectionFactory() {
+        try {
+            props.load(new FileReader("src/main/resources/application.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ConnectionFactory getInstance() {
+        if (connectionFactory == null) {
+            connectionFactory = new ConnectionFactory();
+        }
+
+        return connectionFactory;
+    }
+
+    public static Connection setConnection(String hostURL, String username, String password, String schemaName) {
+
+        String processedHostURL = "jdbc:mysql://" + hostURL + "/postgres?schemaname:" + schemaName;
+        props.put("host-url", processedHostURL);
+        props.put("username", username);
+        props.put("password", password);
+
+        return null;
+    }
+
+
+    }
+
