7a6261c4f6c2a0df8d7559735ca5ccb4418a9738
diuguide <everett.diuguid@gmail.com>
2021-05-31 14:39:40 -0400

    up to 67%

--- src/test/java/com/revature/services/TaskServiceTest.java
+++ src/test/java/com/revature/services/TaskServiceTest.java
@@ -52,128 +52,145 @@ public class TaskServiceTest {
     @Mock
     StatementBuilder mockStatementBuilder;
 
-@Before
-public void before() throws Exception { openMocks(this); }
+    @Before
+    public void before() throws Exception {
+        openMocks(this);
+    }
 
-@After
-public void after() throws Exception {
-    sut = null;
-    mockResultSetDTO = null;
-    rs = null;
-    mockStatementBuilder = null;
-    mockStatementType = null;
-} 
+    @After
+    public void after() throws Exception {
+        sut = null;
+        mockResultSetDTO = null;
+        rs = null;
+        mockStatementBuilder = null;
+        mockStatementType = null;
+    }
 
-@Test (expected = MessageLengthOutOfBoundsException.class)
+    @Test(expected = MessageLengthOutOfBoundsException.class)
     public void testMessageLengthValidation() throws SQLException, ImproperConfigurationException {
-    Task task = new Task();
-    task.setTaskId(500);
-    String message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris viverra varius sem, nec accumsan risus pretium sit amet. Nulla laoreet risus vel quam tristique porta. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per massa nunc. Words";
-    sut.updateTaskContent(task.getTaskId(), message);
-}
+        Task task = new Task();
+        task.setTaskId(500);
+        String message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris viverra varius sem, nec accumsan risus pretium sit amet. Nulla laoreet risus vel quam tristique porta. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per massa nunc. Words";
+        sut.updateTaskContent(task.getTaskId(), message);
+    }
 
-@Test (expected = TitleLengthOutOFBoundsException.class)
+    @Test(expected = TitleLengthOutOFBoundsException.class)
     public void testTitleLengthOutOfBounds() throws SQLException, ImproperConfigurationException {
-    Task task = new Task();
-    task.setTaskId(500);
-    String title = "Very long super long dude so long title man this is so long its the longest title ever.";
-    sut.updateTaskTitle(task.getTaskId(), title);
-}
+        Task task = new Task();
+        task.setTaskId(500);
+        String title = "Very long super long dude so long title man this is so long its the longest title ever.";
+        sut.updateTaskTitle(task.getTaskId(), title);
+    }
 
-@Test (expected = DateFormatException.class)
+    @Test(expected = DateFormatException.class)
     public void testDateOutOfFormatException() throws SQLException, ImproperConfigurationException {
-    Task task = new Task();
-    task.setTaskId(500);
-    String date = "113/223/29";
-    sut.updateTaskDueDate(task.getTaskId(), date);
-}
+        Task task = new Task();
+        task.setTaskId(500);
+        String date = "113/223/29";
+        sut.updateTaskDueDate(task.getTaskId(), date);
+    }
 
-@Test
+    @Test
     public void validTaskMessageUpdate() {
-    Task task = new Task();
-    task.setTaskId(500);
-    task.setTaskMessage("This is a perfectly valid message");
-    try (MockedStatic<ORMState> mockORMstate = Mockito.mockStatic(ORMState.class)) {
-        mockORMstate.when((MockedStatic.Verification) ORMState.getStatementBuilder(any())).thenReturn(mockStatementBuilder);
-        when(ORMState.getStatementBuilder("update")).thenReturn(mockStatementBuilder);
-        when(mockStatementBuilder.buildStatement(any())).thenReturn(rs);
-        when(mockStatementType.UPDATE.createStatementWithCondition(any())).thenReturn(rs);
-        when(mockResultSetDTO.resultSetForSingleTask(any())).thenReturn(task);
-        assertTrue(sut.updateTaskContent(task.getTaskId(), task.getTaskMessage()));
-    } catch (Exception e) {
-        e.printStackTrace();
+        Task task = new Task();
+        task.setTaskId(500);
+        task.setTaskMessage("This is a perfectly valid message");
+        try (MockedStatic<ORMState> mockORMstate = Mockito.mockStatic(ORMState.class)) {
+            mockORMstate.when((MockedStatic.Verification) ORMState.getStatementBuilder(any())).thenReturn(mockStatementBuilder);
+            when(ORMState.getStatementBuilder("update")).thenReturn(mockStatementBuilder);
+            when(mockStatementBuilder.buildStatement(any())).thenReturn(rs);
+            when(mockStatementType.UPDATE.createStatementWithCondition(any())).thenReturn(rs);
+            when(mockResultSetDTO.resultSetForSingleTask(any())).thenReturn(task);
+            assertTrue(sut.updateTaskContent(task.getTaskId(), task.getTaskMessage()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-}
 
-@Test
+    @Test
     public void validTaskTitleUpdate() {
-    Task task = new Task();
-    task.setTaskId(500);
-    task.setTaskTitle("perfectly valid title");
-    try (MockedStatic<ORMState> mockORMstate = Mockito.mockStatic(ORMState.class)) {
-        mockORMstate.when((MockedStatic.Verification) ORMState.getStatementBuilder(any())).thenReturn(mockStatementBuilder);
-        when(ORMState.getStatementBuilder("update")).thenReturn(mockStatementBuilder);
-        when(mockStatementBuilder.buildStatement(any())).thenReturn(rs);
-        when(mockStatementType.UPDATE.createStatementWithCondition(any())).thenReturn(rs);
-        when(mockResultSetDTO.resultSetForSingleTask(any())).thenReturn(task);
-        assertTrue(sut.updateTaskContent(task.getTaskId(), task.getTaskTitle()));
-    } catch (Exception e) {
-        e.printStackTrace();
+        Task task = new Task();
+        task.setTaskId(500);
+        task.setTaskTitle("perfectly valid title");
+        try (MockedStatic<ORMState> mockORMstate = Mockito.mockStatic(ORMState.class)) {
+            mockORMstate.when((MockedStatic.Verification) ORMState.getStatementBuilder(any())).thenReturn(mockStatementBuilder);
+            when(ORMState.getStatementBuilder("update")).thenReturn(mockStatementBuilder);
+            when(mockStatementBuilder.buildStatement(any())).thenReturn(rs);
+            when(mockStatementType.UPDATE.createStatementWithCondition(any())).thenReturn(rs);
+            when(mockResultSetDTO.resultSetForSingleTask(any())).thenReturn(task);
+            assertTrue(sut.updateTaskContent(task.getTaskId(), task.getTaskTitle()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-}
 
-@Test
+    @Test
     public void validTaskDueDateUpdate() {
-    Task task = new Task();
-    task.setTaskId(500);
-    task.setDateDue("1/11/2010");
-    try (MockedStatic<ORMState> mockORMstate = Mockito.mockStatic(ORMState.class)) {
-        mockORMstate.when((MockedStatic.Verification) ORMState.getStatementBuilder(any())).thenReturn(mockStatementBuilder);
-        when(ORMState.getStatementBuilder("update")).thenReturn(mockStatementBuilder);
-        when(mockStatementBuilder.buildStatement(any())).thenReturn(rs);
-        when(mockStatementType.UPDATE.createStatementWithCondition(any())).thenReturn(rs);
-        when(mockResultSetDTO.resultSetForSingleTask(any())).thenReturn(task);
-        assertTrue(sut.updateTaskContent(task.getTaskId(), task.getDateDue()));
-    } catch (Exception e) {
-        e.printStackTrace();
+        Task task = new Task();
+        task.setTaskId(500);
+        task.setDateDue("1/11/2010");
+        try (MockedStatic<ORMState> mockORMstate = Mockito.mockStatic(ORMState.class)) {
+            mockORMstate.when((MockedStatic.Verification) ORMState.getStatementBuilder(any())).thenReturn(mockStatementBuilder);
+            when(ORMState.getStatementBuilder("update")).thenReturn(mockStatementBuilder);
+            when(mockStatementBuilder.buildStatement(any())).thenReturn(rs);
+            when(mockStatementType.UPDATE.createStatementWithCondition(any())).thenReturn(rs);
+            when(mockResultSetDTO.resultSetForSingleTask(any())).thenReturn(task);
+            assertTrue(sut.updateTaskContent(task.getTaskId(), task.getDateDue()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-}
 
-@Test
+    @Test
     public void validTaskState() {
-    Task task = new Task();
-    task.setTaskId(500);
-    try (MockedStatic<ORMState> mockORMstate = Mockito.mockStatic(ORMState.class)) {
-        mockORMstate.when((MockedStatic.Verification) ORMState.getStatementBuilder(any())).thenReturn(mockStatementBuilder);
-        when(ORMState.getStatementBuilder("update")).thenReturn(mockStatementBuilder);
-        when(mockStatementBuilder.buildStatement(any())).thenReturn(rs);
-        when(mockStatementType.UPDATE.createStatementWithCondition(any())).thenReturn(rs);
-        when(mockResultSetDTO.resultSetForSingleTask(any())).thenReturn(task);
-        assertTrue(sut.updateTaskState(task.getTaskId()));
-    } catch (Exception e) {
-        e.printStackTrace();
+        Task task = new Task();
+        task.setTaskId(500);
+        try (MockedStatic<ORMState> mockORMstate = Mockito.mockStatic(ORMState.class)) {
+            mockORMstate.when((MockedStatic.Verification) ORMState.getStatementBuilder(any())).thenReturn(mockStatementBuilder);
+            when(ORMState.getStatementBuilder("update")).thenReturn(mockStatementBuilder);
+            when(mockStatementBuilder.buildStatement(any())).thenReturn(rs);
+            when(mockStatementType.UPDATE.createStatementWithCondition(any())).thenReturn(rs);
+            when(mockResultSetDTO.resultSetForSingleTask(any())).thenReturn(task);
+            assertTrue(sut.updateTaskState(task.getTaskId()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
-}
-
-@Test
+    @Test
     public void testMessageLengthError() throws SQLException, ImproperConfigurationException {
         Task task = new Task();
         task.setTaskId(500);
         String message = " Test phrase";
-        when(mockStatementBuilder.buildStatement(any(),any())).thenThrow(new SQLException());
-        sut.updateTaskContent(task.getTaskId(), message);
+        try (MockedStatic<ORMState> mockORMstate = Mockito.mockStatic(ORMState.class)) {
+            mockORMstate.when((MockedStatic.Verification) ORMState.getStatementBuilder(any())).thenReturn(mockStatementBuilder);
+            when(ORMState.getStatementBuilder("update")).thenReturn(mockStatementBuilder);
+            when(mockStatementBuilder.buildStatement(any(), any())).thenThrow(new SQLException());
+            when(mockResultSetDTO.resultSetForSingleTask(any())).thenReturn(task);
+            sut.updateTaskContent(task.getTaskId(), message);
+        } catch (ImproperConfigurationException e) {
+
+        }
     }
 
-@Test
-    public void taskLengthError() throws SQLException, ImproperConfigurationException {
-        Task task = new Task();
-        task.setTaskId(500);
-        String title = "Test phrase";
-        when(mockStatementBuilder.buildStatement(any(),any())).thenThrow(new SQLException());
-        sut.updateTaskTitle(task.getTaskId(), title);
+        @Test
+        public void taskLengthError () throws SQLException, ImproperConfigurationException {
+            Task task = new Task();
+            task.setTaskId(500);
+            String title = "Test phrase";
+            try (MockedStatic<ORMState> mockORMstate = Mockito.mockStatic(ORMState.class)) {
+                mockORMstate.when((MockedStatic.Verification) ORMState.getStatementBuilder(any())).thenReturn(mockStatementBuilder);
+                when(ORMState.getStatementBuilder("update")).thenReturn(mockStatementBuilder);
+                when(mockStatementBuilder.buildStatement(any(), any())).thenThrow(new SQLException());
+                when(mockResultSetDTO.resultSetForSingleTask(any())).thenReturn(task);
+                sut.updateTaskTitle(task.getTaskId(), title);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-}
+        }
 
-}
+    }
+
 
